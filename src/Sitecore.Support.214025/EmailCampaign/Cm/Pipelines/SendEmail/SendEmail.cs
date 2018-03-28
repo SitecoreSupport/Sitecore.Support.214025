c4e16dae8d1ec560d575789652479fdb19bf0019
Sergey Plashenko <spl@sitecore.net>
2018-03-28 12:48:44 +0300

    Fix for the issue

---
+++ src/Sitecore.Support.214025/EmailCampaign/Cm/Pipelines/SendEmail/SendEmail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sitecore.EmailCampaign.Cm.Pipelines.SendEmail;
+using Sitecore.Modules.EmailCampaign.Core;
+
+namespace Sitecore.Support.EmailCampaign.Cm.Pipelines.SendEmail
+{
+  public class SendEmail : Sitecore.EmailCampaign.Cm.Pipelines.SendEmail.SendEmail
+  {
+    public SendEmail(Sitecore.ExM.Framework.Diagnostics.ILogger logger) : base(logger)
+    {
+    }
+
+    public SendEmail(Sitecore.ExM.Framework.Diagnostics.ILogger logger, Sitecore.EDS.Core.Dispatch.IDispatchManager dispatchManager, Sitecore.EmailCampaign.Model.Web.Settings.EcmSettings exmSettings) : base(logger, dispatchManager, exmSettings)
+    {
+    }
+
+    public new void Process(SendMessageArgs args)
+    {
+      base.Process(args);
+      if (args.IsTestSend && args.StartSendTime > DateTime.MinValue)
+      {
+        EcmGlobals.GenerationSemaphore.ReleaseOne();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.214025/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.214025")]
+[assembly: AssemblyProduct("Sitecore.Support.214025")]
+[assembly: ComVisible(false)]
